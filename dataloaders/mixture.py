805a15e96758d27f4053940d46e4f00caf4cb3f8
NguyenTriQuan <nguyentriquan99@gmail.com>
2022-11-08 23:39:48 +0700

    sccl

--- dataloaders/mixture.py
+++ dataloaders/mixture.py
@@ -45,7 +45,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
 
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.targets)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.targets)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 3
             train_data = train_data.permute(0, 3, 1, 2)/255.0
             test_data = test_data.permute(0, 3, 1, 2)/255.0
             # print(idx, train_data.shape)
@@ -72,7 +72,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=100
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.targets)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.targets)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 3
             train_data = train_data.permute(0, 3, 1, 2)/255.0
             test_data = test_data.permute(0, 3, 1, 2)/255.0
             # print(idx, train_data.shape)
@@ -101,7 +101,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=10
             train_data, train_targets = train_set.data.float(), train_set.targets.long()
             test_data, test_targets = test_set.data.float(), test_set.targets.long()
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 1
             train_data = train_data.unsqueeze(1)/255.0
             test_data = test_data.unsqueeze(1)/255.0
             train_data = train_data.expand(train_data.size(0), 3, train_data.size(2), train_data.size(3))
@@ -134,7 +134,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=10
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.labels)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.labels)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 3
             train_data = train_data/255.0
             test_data = test_data/255.0
             # print(idx, train_data.shape)
@@ -161,7 +161,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=10
             train_data, train_targets = train_set.data.float(), train_set.targets.long()
             test_data, test_targets = test_set.data.float(), test_set.targets.long()
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 1
             train_data = train_data.unsqueeze(1)/255.0
             test_data = test_data.unsqueeze(1)/255.0
             train_data = train_data.expand(train_data.size(0), 3, train_data.size(2), train_data.size(3))
@@ -195,7 +195,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=43
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.labels)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.labels)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 3
             train_data = train_data/255.0
             test_data = test_data/255.0
             # print(idx, train_data.shape)
@@ -222,7 +222,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=100
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.labels)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.labels)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 3
             train_data = train_data/255.0
             test_data = test_data/255.0
             # print(idx, train_data.shape)
@@ -249,7 +249,7 @@ def get(args, pc_valid=0.15,fixed_order=False):
             data[n]['ncla']=10
             train_data, train_targets = torch.FloatTensor(train_set.data), torch.LongTensor(train_set.labels)
             test_data, test_targets = torch.FloatTensor(test_set.data), torch.LongTensor(test_set.labels)
-            data[n]['factor'] = train_data.numel()
+            data[n]['factor'] = train_data.size(0) * 1
             train_data = train_data/255.0
             test_data = test_data/255.0
             train_data = train_data.expand(train_data.size(0), 3, train_data.size(2), train_data.size(3))

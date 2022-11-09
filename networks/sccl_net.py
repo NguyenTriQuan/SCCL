e3e6aa12f297950f2b04656ffec0f3543cd75f82
NguyenTriQuan <nguyentriquan99@gmail.com>
2022-11-10 05:21:09 +0700

    sccl

--- networks/sccl_net.py
+++ networks/sccl_net.py
@@ -294,10 +294,10 @@ class Alexnet(_DynamicModel):
             nn.Flatten(),
             DynamicLinear(256*s*s, 2048, s=s, norm_type=norm_type),
             nn.ReLU(),
-            # nn.Dropout(0.5),
+            nn.Dropout(0.5),
             DynamicLinear(2048, 2048, norm_type=norm_type),
             nn.ReLU(),
-            # nn.Dropout(0.5),
+            nn.Dropout(0.5),
             DynamicLinear(2048, 0, last_layer=True, norm_type=norm_type)
         ])
         self.DM = [m for m in self.modules() if isinstance(m, _DynamicLayer)]

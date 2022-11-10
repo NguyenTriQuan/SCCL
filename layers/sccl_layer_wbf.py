1588ad74a5a2fc623dd5e26d190a5d16e35a3c41
NguyenTriQuan <nguyentriquan99@gmail.com>
2022-11-10 19:34:28 +0700

    sccl

--- layers/sccl_layer_wbf.py
+++ layers/sccl_layer_wbf.py
@@ -224,8 +224,8 @@ class _DynamicLayer(nn.Module):
             self.strength_out.append(strength_out)
         self.strength_out = sum(self.strength_out)
 
-        self.strength_in = self.strength_in ** 0.5
-        self.strength_out = self.strength_out ** 0.5
+        # self.strength_in = self.strength_in ** 0.5
+        # self.strength_out = self.strength_out ** 0.5
 
         self.strength = (self.strength_in + self.strength_out)
 
